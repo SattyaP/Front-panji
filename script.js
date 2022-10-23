8a61865de5d74098af80d70061fda02bb8d705e2
sattyap <stylearning7@gmail.com>
2022-10-23 18:12:26 +0700

    adding new page

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let hero = document.querySelector(".btn-hero").getBoundingClientRect(),
     heroHover = document.querySelector(".btn-hero-hover");
 heroHover.style.transform = `translate(${hero.x}px, ${hero.y}px)`
 heroHover.classList.add("hero-transform")
+
+// var a;
+// function show()
+// {
+//     if(a==1)
+//     {
+//         document.getElementById("crd-event").style.display="block"
+//         return a=0;
+//     }
+//     else 
+//     {
+//         document.getElementById("crd-event").style.display="none";
+//         return a=1;
+//     }
+// }
+
+// Nanti dulu ini
