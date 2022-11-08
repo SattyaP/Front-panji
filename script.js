90ea786671f4104d94ee9aa57ec3c5d62483891c
sattyap <stylearning7@gmail.com>
2022-11-08 08:26:43 +0700

    fix button

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let hero = document.querySelector(".btn-hero").getBoundingClientRect(),
-    heroHover = document.querySelector(".btn-hero-hover");
-heroHover.style.transform = `translate(${hero.x}px, ${hero.y}px)`
-heroHover.classList.add("hero-transform")
 
 // var a;
 // function show()
